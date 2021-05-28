7cbcdafac36116a45962d6e00d1f5f5ea9f61377
Robert Kruszewski <dzobert@gmail.com>
2021-05-28 19:02:02 +0100

    something

--- spending-server/src/main.rs
+++ spending-server/src/main.rs
@@ -4,6 +4,9 @@ use warp::Filter;
 
 mod resource;
 
+use resource::oauth;
+use std::sync::Arc;
+
 #[tokio::main]
 async fn main() {
     // GET /hello/warp => 200 OK with body "Hello, warp!"
@@ -12,9 +15,9 @@ async fn main() {
         .map(|name: String| format!("Hello, {}!", name));
 
     let oauth2 =
-        resource::oauth::Oauth2Resource::new(resource::oauth::Oauth2Config::new("", "", "", ""));
+        Arc::new(oauth::Oauth2Resource::new(resource::oauth::Oauth2Config::new("", "", "", "")));
 
-    warp::serve(hello.or(oauth2.build_routes()))
+    warp::serve(hello.or(oauth::routes(&oauth2)))
         .run(([127, 0, 0, 1], 3030))
         .await;
 }
--- spending-server/src/resource/oauth.rs
+++ spending-server/src/resource/oauth.rs
@@ -3,16 +3,14 @@ use oauth2::basic::BasicClient;
 use oauth2::reqwest::async_http_client;
 use oauth2::{
     AuthUrl, AuthorizationCode, ClientId, ClientSecret, CsrfToken, PkceCodeChallenge, RedirectUrl,
-    Scope, TokenResponse, TokenUrl,
+    Scope, TokenUrl,
 };
-use std::future::Future;
 use warp::filters::BoxedFilter;
-use warp::http::Error;
 use warp::http::Response;
-use warp::http::Uri;
 use warp::Filter;
 use warp::Rejection;
 use warp::Reply;
+use std::sync::Arc;
 
 pub struct Oauth2Config<'a> {
     client_id: &'a str,
@@ -47,71 +45,89 @@ impl<'a> Oauth2Config<'a> {
     }
 }
 
-pub struct Oauth2Resource<'a> {
-    config: Oauth2Config<'a>,
-    client: BasicClient,
+pub struct Oauth2Resource {
+    oauth2_client: Arc<BasicClient>,
 }
 
-impl<'a> Oauth2Resource<'a> {
-    pub fn new(config: Oauth2Config<'a>) -> Oauth2Resource<'a> {
+impl Oauth2Resource {
+    pub fn new(config: Oauth2Config<'_>) -> Oauth2Resource {
         return Oauth2Resource {
-            config: config,
-            client: config.oauth2_client(),
+            oauth2_client: Arc::new(config.oauth2_client()),
         };
     }
+}
 
-    pub fn build_routes(&'static self) -> BoxedFilter<(impl Reply,)> {
-        let login_filter = warp::path("oauth2/login")
-            .map(move || {
-                let (pkce_challenge, pkce_verifier) = PkceCodeChallenge::new_random_sha256();
+pub fn routes(oauth2_resource: &Arc<Oauth2Resource>) -> BoxedFilter<(impl Reply,)> {
+    let login_filter = warp::path("oauth2/login")
+        .and(warp::path::end())
+        .and_then({
+            let client = Arc::clone(&oauth2_resource.oauth2_client);
+            move || {
+                let client = Arc::clone(&client);
+                return login(client);
+            }
+        })
+        .boxed();
 
-                // Generate the full authorization URL.
-                let (auth_url, csrf_token) = self
-                    .client
-                    .authorize_url(CsrfToken::new_random)
-                    // Set the desired scopes.
-                    .add_scope(Scope::new("read".to_string()))
-                    .add_scope(Scope::new("write".to_string()))
-                    // Set the PKCE code challenge.
-                    .set_pkce_challenge(pkce_challenge)
-                    .url();
+    let redirect = warp::path("oauth2/redirect")
+        .and(warp::path::end())
+        .and_then(
+            
+            {
+                let client = Arc::clone(&oauth2_resource.oauth2_client);
+                move || {
+                    let client = Arc::clone(&client);
+                    return redirect(client);
+                }
+            }
+        )
+        .boxed();
 
-                let mut csrf_cookie = String::from("csrf_token=");
-                csrf_cookie.push_str(csrf_token.secret());
+    return login_filter.or(redirect).boxed();
+}
 
-                return Response::builder()
-                    .header(header::SET_COOKIE, csrf_cookie)
-                    .header(header::LOCATION, auth_url.as_str())
-                    .status(StatusCode::MOVED_PERMANENTLY)
-                    .body(vec![]);
-            })
-            .boxed();
+async fn login(oauth2_client: Arc<BasicClient>) -> Result<Response<Vec<u8>>, Rejection> {
+    let (pkce_challenge, pkce_verifier) = PkceCodeChallenge::new_random_sha256();
 
-        let redirect = warp::path("oauth2/redirect")
-            .and_then(async move || {
-                // Once the user has been redirected to the redirect URL, you'll have access to the
-                // authorization code. For security reasons, your code should verify that the `state`
-                // parameter returned by the server matches `csrf_state`.
+    // Generate the full authorization URL.
+    let (auth_url, csrf_token) = oauth2_client
+        .authorize_url(CsrfToken::new_random)
+        // Set the desired scopes.
+        .add_scope(Scope::new("read".to_string()))
+        .add_scope(Scope::new("write".to_string()))
+        // Set the PKCE code challenge.
+        .set_pkce_challenge(pkce_challenge)
+        .url();
 
-                // Now you can trade it for an access token.
-                let token_result = self
-                    .client
-                    .exchange_code(AuthorizationCode::new(
-                        "some authorization code".to_string(),
-                    ))
-                    // // Set the PKCE code verifier.
-                    // .set_pkce_verifier(pkce_verifier)
-                    .request_async(async_http_client)
-                    .await
-                    .unwrap();
+    let mut csrf_cookie = String::from("csrf_token=");
+    csrf_cookie.push_str(csrf_token.secret());
 
-                return Response::builder()
-                    .status(StatusCode::OK)
-                    .body(vec![])
-                    .map_err(|_err| warp::reject::reject());
-            })
-            .boxed();
+    return Response::builder()
+        .header(header::SET_COOKIE, csrf_cookie)
+        .header(header::LOCATION, auth_url.as_str())
+        .status(StatusCode::MOVED_PERMANENTLY)
+        .body(vec![])
+        .map_err(|_err| warp::reject::reject());
+}
 
-        return login_filter.or(redirect).boxed();
-    }
+async fn redirect(oauth2_client: Arc<BasicClient>) -> Result<Response<Vec<u8>>, Rejection> {
+    // Once the user has been redirected to the redirect URL, you'll have access to the
+    // authorization code. For security reasons, your code should verify that the `state`
+    // parameter returned by the server matches `csrf_state`.
+
+    // Now you can trade it for an access token.
+    let token_result = oauth2_client
+        .exchange_code(AuthorizationCode::new(
+            "some authorization code".to_string(),
+        ))
+        // // Set the PKCE code verifier.
+        // .set_pkce_verifier(pkce_verifier)
+        .request_async(async_http_client)
+        .await
+        .unwrap();
+
+    return Response::builder()
+        .status(StatusCode::OK)
+        .body(vec![])
+        .map_err(|_err| warp::reject::reject());
 }
